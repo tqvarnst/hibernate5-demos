27834430f079ff6d637b7d49f1b4daa480baf123
tqvarnst <tqvarnst@redhat.com>
2016-05-12 16:58:26 +0200

    Switch to using Hypersonic and ignored the StoreProcedure test

--- projects/hibernate-demo-app/src/test/resources/sp.sql
+++ projects/hibernate-demo-app/src/test/resources/sp.sql
@@ -20,6 +20,6 @@
 --END;
 --$$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION my_sum(x int, y int, OUT sum int) AS 'SELECT x+y' LANGUAGE SQL;	
+--CREATE OR REPLACE FUNCTION my_sum(x int, y int, OUT sum int) AS 'SELECT x+y' LANGUAGE SQL;	
 
-CREATE OR REPLACE FUNCTION add_three_values(v1 int, v2 int, v3 int, OUT sum int) AS 'SELECT v1+v2+v3' LANGUAGE SQL;
+--CREATE OR REPLACE FUNCTION add_three_values(v1 int, v2 int, v3 int, OUT sum int) AS 'SELECT v1+v2+v3' LANGUAGE SQL;
